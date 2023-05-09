857d85e56358301aa76180844aab4ef5c2278066
Bhavitha358 <130962932+Bhavitha358@users.noreply.github.com>
2023-05-09 13:28:53 +0530

    Create 9 may session.java

---
+++ 9 may session.java
@@ -0,0 +1,22 @@
+OBJECT ORIENTED PROGRAMMING
+--> objects
+--> classes
+--> variables
+--> methods
+Constructors :
+1) default constructors 
+2) parameterized constructors
+If we didn't initialize any variable in the method, then by default these are assigned with values as follows:
+  string--> null
+  int --> 0
+  boolean --> false
+Access specifiers :
+  public
+  private
+--> By creating methods in the class we can access private members of the class.
+ Keywords :
+-->This: refers to the current instance of the class and is used to access variable that belong to the class.
+   * also used when the attributes and data members share the same name.
+--> Final: is used to fix the value of the variable, properties or methods.
+  * If any variable is declared as a final so you can't set the value or modify the value later.
+ -->static : if a method or variable is given as static, then that method or variable belongs to entire class.
